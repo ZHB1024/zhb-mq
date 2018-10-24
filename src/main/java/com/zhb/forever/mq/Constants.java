60c741d1f1d193cf46a82939ad800b6525b506cc
ZHB-JOB <zhb20111503@126.com>
2018-10-24 13:50:50 +0800

    destination name

--- src/main/java/com/zhb/forever/mq/Constants.java
+++ src/main/java/com/zhb/forever/mq/Constants.java
@@ -6,5 +6,8 @@ public class Constants {
     public static String MQ_DESTINATION = "queueDestination";
     
     public static String MQ_CLIENT_CONF = "mq-client.xml";
+    
+    public static final String DEFAULT_DESTINATION_NAME = "zhb_vue";
+    public static final String KEY_VALUE_DESTINATION_NAME = "key_value_protobuf";
 
 }
